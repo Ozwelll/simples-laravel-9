edc34015cbed4af7fd5c0f18a49c6361ae77a9eb
Ozwelll <vbtelecom27@gmail.com>
2022-10-21 05:37:06 -0300

    add relacionamento de comentarios

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,5 +42,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 
 }
